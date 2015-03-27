643b13e418c4265ca4642b5152ac7cb666fbc8d2
Andrea Planells <andyplanells@unitec.edu>
2015-03-27 14:29:48 -0600

    Adding up stuff to vectors, creation complete!

--- main.cpp
+++ main.cpp
@@ -9,11 +9,18 @@
 using namespace std;
 
 int menu();
-void CreateProduct(vector<Product*>&);
+void CreateProduct(vector<Product*>&,double,double,double);
 
 int main(int argc, char* argv[]){
 	int option = menu();
 	vector<Product*> myproducts;
+	double ratescholar, ratealcohol, rateluxury;
+	cout<<"Welcome!\nPlease introduce the following:\n Rate for Scholar products: ";
+	cin>>ratescholar;
+	cout<<"Rate for Alcoholic products: ";
+	cin>>ratealcohol;
+	cout<<"Rate for Luxury Products: ";
+	cin>>rateluxury;
 	while(option<3){
 		if(option==1){
 
@@ -33,7 +40,7 @@ int menu(){
 	return option;
 }
 
-void CreateProduct(vector<Product*>& myproducts){
+void CreateProduct(vector<Product*>& myproducts, double ratescholar, double ratealcohol, double rateluxury){
 	string client;
 	int option;
 	double price, volume, weight;
@@ -59,6 +66,17 @@ void CreateProduct(vector<Product*>& myproducts){
 			cout<<"Invalid option, please choose between 1 and 3!\nChoose: ";
 			cin>>option;
 		}
+	}
+
+	if(type=="Scholar"){
+		//double price, double volume, double weight, string client, double rate
+		myproducts.push_back(new Scholar(price,volume,weight,client,ratescholar));
+	}else if(type=="Alcohol"){
+		//double price, double volume, double weight, string client, double rate
+		myproducts.push_back(new Alcohol(price,volume,weight,client,ratealcohol));
+	}else{
+		//double price, double volume, double weight, string client, double rate
+		myproducts.push_back(new Luxury(price,volume,weight,client,rateluxury));
 	}
 
 }
